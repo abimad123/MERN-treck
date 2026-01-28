8ad74c45f57f6133927a1b098a1a07cc4b9e8913
Abijith CG <abijithc140@gmail.com>
2026-01-28 10:50:41 +0530

    Add Footer and Navbar components
    - Created a Footer component that displays a simple footer with a heading.
    - Created a Navbar component with links for Home and About sections.

--- client/startingreact/src/App.jsx
+++ client/startingreact/src/App.jsx
@@ -1,38 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import './App.css';
+import Footer from './components/footer';
+import Navbar from "./components/navbar";
+export default function App(){
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <h1>
-          So we are starting our React journey with Vite!
-        </h1>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+   3  <Main />
+    <Footer />
     </>
-  )
+  );
 }
 
-export default App
+
+
+// export default App;
+
+// const name="abi";
+// console.log(name);
+// export {name} //to export an single thing
---
+++ client/startingreact/src/components/navbar.jsx
@@ -0,0 +1,11 @@
+export default function navbar (){
+    return(
+        <>
+         <div>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href=""></a>
+         </div>
+        </>
+    );
+}
