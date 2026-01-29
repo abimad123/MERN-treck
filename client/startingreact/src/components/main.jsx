d45786004ea8bb682c0e66652c66b5cc15745bdf
Abijith CG <abijithc140@gmail.com>
2026-01-29 11:15:35 +0530

    added tailwindcss

--- client/startingreact/src/components/main.jsx
+++ client/startingreact/src/components/main.jsx
@@ -1,26 +1,35 @@
-import {useState} from "react";
-const Main=()=>{
-    const [count, setCount] = useState(0);
-    const incerment =(c)=>{
-        setCount(count+c)
-    };
+import { useState } from "react";
 
-    let value =3;
-    return(<div>
-        <form action="">
-            <label htmlFor="incermenter">
-                Incermenter:
-            </label>
-            <input id="incermenter" 
-            type="number"
-            required 
-            value={value} 
-            onChange={(e)=>value=e.target.value}
-            />
-        <button onClick={()=>incerment(2)}> count:{count} Increment</button>
-        </form>
-        
+const Main = () => {
+  const [count, setCount] = useState(0);
+  const [value, setValue] = useState();
+
+  const increment = (c) => {
+    // setCount(count + c);
+    setCount((prev) => prev + c);
+  };
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    increment(value);
+  }
+  return (
+    <div className="">
+      count: {count}
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="incrementer">Incrementer:</label>
+        <input 
+          id="incrementer"
+          type="number"
+          required
+          value={value}
+          onChange={(e)=> setValue(Number(e.target.value))}
+        />
+        <button>Increment</button>
+        {/* <button onClick={() => increment(value)}>Increment</button> */}
+      </form>
     </div>
-    );
-}
+  );
+};
+
 export default Main;
